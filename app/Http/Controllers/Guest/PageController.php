b39254618e7f30083fc0f8cb2e9742967b6d1af0
TommasoBernardo <tommasob1210@gmail.com>
2023-02-12 12:39:08 +0100

    recreate repo

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    //inserire la funzione di index per la rotta
+
+    public function index()
+    {
+
+        $movies = Movie::all();
+        return view('home', ['movies' => $movies]);
+    }
 }
